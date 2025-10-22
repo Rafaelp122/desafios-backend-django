3093022b219787fa64bc24dd0000fb4cd1759309
Rafael Pereira <rafaelpereiradearaujo5@gmail.com>
2025-10-22 20:59:53 -0300

    Adiciona view e rotas para Produtos com filtro por categoria

---
+++ desafio-02-api-produtos/produtos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ProdutoListCreateView
+
+
+urlpatterns = [
+    path('produtos/', ProdutoListCreateView.as_view(), name='produto-list-create'),
+]
--- desafio-02-api-produtos/produtos/views.py
+++ desafio-02-api-produtos/produtos/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Produto
+from .serializers import ProdutoSerializer
 
-# Create your views here.
+
+class ProdutoListCreateView(generics.ListCreateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+    def get_queryset(self):
+        queryset = Produto.objects.all()
+        categoria = self.request.query_params.get('categoria')
+
+        if categoria:
+            queryset = queryset.filter(categoria=categoria)
+
+        return queryset
